5e9ef7ff05b8e68b5f83728ec66ff190e38cf388
gituser00001 <lptvsergey@yandex.ru>
2022-07-25 13:33:26 +0300

    add logic

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -2,5 +2,35 @@
 
 namespace BrainGames\Games\Calc;
 
-use function cli\line;
-use function cli\prompt;
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = 'What is the result of the expression?';
+const MIN_NUM = 1;
+const MAX_NUM = 50;
+const OPERATION = ['+', '-', '*'];
+
+function calculate(int $num1, int $num2, string $operation)
+{
+    switch ($operation) {
+        case '+':
+            return $num1 + $num2;
+        case '-':
+            return $num1 - $num2;
+        case '*':
+            return $num1 * $num2;
+    }
+}
+
+function play()
+{
+    $roundData = function () {
+        $randNum1 = rand(MIN_NUM, MAX_NUM);
+        $randNum2 = rand(MIN_NUM, MAX_NUM);
+        $operation = OPERATION[array_rand(OPERATION)];
+        return [
+        'question' => "$randNum1 $operation $randNum2",
+        'answer' => (string) calculate($randNum1, $randNum2, $operation)
+        ];
+    };
+    run(DESCRIPTION, $roundData);
+}
