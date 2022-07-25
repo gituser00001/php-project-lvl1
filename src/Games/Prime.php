4160f13f65cbd712db93e2445ec92e3f53849e28
gituser00001 <lptvsergey@yandex.ru>
2022-07-25 22:06:57 +0300

    added logic

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -7,3 +7,36 @@ use function BrainGames\Engine\run;
 const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const MIN_NUM = 1;
 const MAX_NUM = 100;
+
+function isPrime($number)
+{
+    if ($number < 2) {
+        return false;
+    }
+
+    $divisor = 2;
+
+    while ($divisor <= $number / 2) {
+        if ($number % $divisor === 0) {
+            return false;
+        }
+
+        $divisor += 1;
+    }
+
+    return true;
+}
+
+function play()
+{
+    $roundData = function () {
+        $randomNum = rand(MIN_NUM, MAX_NUM);
+        $question = "$randomNum";
+        $answer = isPrime($randomNum) ? 'yes' : 'no';
+        return [
+            'question' => $question,
+            'answer' => $answer
+        ];
+    };
+    run(DESCRIPTION, $roundData);
+}
