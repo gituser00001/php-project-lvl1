9db47b0b59d33d939b1b6a31b83bddca3a5c1d6b
gituser00001 <lptvsergey@yandex.ru>
2022-07-25 16:58:11 +0300

    add script logic

--- src/Games/GCD.php
+++ src/Games/GCD.php
@@ -6,4 +6,29 @@ use function BrainGames\Engine\run;
 
 const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
 const MIN_NUM = 1;
-const MAX_NUM = 50;
+const MAX_NUM = 100;
+
+function gcd($a, $b)
+{
+    while ($b !== 0) {
+        $remainder = $a % $b;
+        $a = $b;
+        $b = $remainder;
+    }
+    return $a;
+}
+
+function play()
+{
+    $roundData = function () {
+        $num1 = rand(MIN_NUM, MAX_NUM);
+        $num2 = rand(MIN_NUM, MAX_NUM);
+        $question = "$num1 $num2";
+        $answer = gcd($num1, $num2);
+        return [
+            'question' => $question,
+            'answer' => $answer
+        ];
+    };
+    run(DESCRIPTION, $roundData);
+}
