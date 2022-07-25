e2c69400c2a1cb9346199d11a66fef97f813ad31
gituser00001 <lptvsergey@yandex.ru>
2022-07-25 13:32:52 +0300

    declare function "run"

--- src/Engine.php
+++ src/Engine.php
@@ -4,3 +4,28 @@ namespace BrainGames\Engine;
 
 use function cli\line;
 use function cli\prompt;
+
+function run(string $description, callable $genRoundData)
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line("$description");
+
+    for ($i = 0; $i < 3; $i++) {
+        ['question' => $question, 'answer' => $correctAnswer] = $genRoundData();
+
+        line(("Question: {$question}"));
+        $answer = prompt('Your answer');
+
+        if ($correctAnswer === $answer) {
+            line('Correct!');
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+            line("Let's try again, %s!", $name);
+            return;
+        }
+    }
+
+    line("Congratulations, %s!", $name);
+}
