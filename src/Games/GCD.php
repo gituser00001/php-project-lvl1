ab6c65c02f2f6dbd9f21e7db089b90901caab8b5
gituser00001 <lptvsergey@yandex.ru>
2022-07-25 16:49:56 +0300

    add new files

---
+++ src/Games/GCD.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BrainGames\Games\GCD;
+
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+const MIN_NUM = 1;
+const MAX_NUM = 50;
