cfa09ed05f3ee8fce71d754de36decc961c3c065
gituser00001 <lptvsergey@yandex.ru>
2022-07-25 21:57:58 +0300

    add new files

---
+++ src/Games/Prime.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BrainGames\Games\Prime;
+
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const MIN_NUM = 1;
+const MAX_NUM = 100;
